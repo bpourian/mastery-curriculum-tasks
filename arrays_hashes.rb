5cb8c5fd0d7b84b473b360160dedd6b5a38c1f8f
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-06 10:07:30 +0000

    arrays_hashes

---
+++ arrays_hashes.rb
@@ -0,0 +1,9 @@
+word = "bear"
+
+dictionary  = {
+	"bear" => "A creature that fishes in the river for salmon.",
+	"river" => "a body of water that contains salmon, and sometimes bears.",
+	"salmon" => "a fish, sometimes in a river, sometimes in a bear, and sometimes in both."
+}
+
+puts dictionary[word]
